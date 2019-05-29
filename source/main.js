fbd7d47cbb7211562fe9ad14fd7d188e937529bb
aportav <andi_idfer@hotmail.com>
2019-05-29 13:59:52 -0600

    Modified UI and done

--- source/main.js
+++ source/main.js
@@ -26,18 +26,82 @@ var numberArray = document.getElementsByClassName("calc-btn-number");
 var operatorArray = document.getElementsByClassName("calc-btn-operator");
 
 // Variables
-var currentNumber = '545657';
-var previousNumber 
-displayVal.innerHTML = '545657';
+var currentNumber = '0';
+var previousNumber;
+var evalStringArray = []; // Will hold the operations we need to compute
 
+// Update UI and assign values to variables when number buttons are clicked 
+var updateDisplyValue = (clickObj) => {
+    var btnText = clickObj.target.innerText;
+    if (currentNumber === '0') {
+        currentNumber = '';
+    }
+
+    currentNumber += btnText;
+    displayVal.innerText = currentNumber;
+}
+
+var performOperation = (clickObj) => {
+    var operator = clickObj.target.innerText;
+
+    switch (operator) {
+        case '+':
+            previousNumber = currentNumber;
+            currentNumber = '0';
+            displayVal.innerText = currentNumber;
+            evalStringArray.push(previousNumber);
+            evalStringArray.push('+');
+            break;
+        case '-':
+            previousNumber = currentNumber;
+            currentNumber = '0';
+            displayVal.innerText = currentNumber;
+            evalStringArray.push(previousNumber);
+            evalStringArray.push('-');        
+            break;
+        case 'x':
+            previousNumber = currentNumber;
+            currentNumber = '0';
+            displayVal.innerText = currentNumber;
+            evalStringArray.push(previousNumber);
+            evalStringArray.push('*');
+            break;
+        case '÷':
+            previousNumber = currentNumber;
+            currentNumber = '0';
+            displayVal.innerText = currentNumber;
+            evalStringArray.push(previousNumber);
+            evalStringArray.push('/');
+            break;
+        case '=':
+            evalStringArray.push(currentNumber);
+            var result = eval(evalStringArray.join(' '));
+            currentNumber = result + '';
+            displayVal.innerText = currentNumber;
+            evalStringArray = [];
+            break;
+        default:
+            break;
+    }
+}
+
+// Create event listeners for each button on the calculator
+for(let i = 0; i < numberArray.length; i++) {
+    numberArray[i].addEventListener('click', updateDisplyValue, false);
+}
+
+
+for(let i = 0; i < operatorArray.length; i++) {
+    operatorArray[i].addEventListener('click', performOperation, false);
+}
 
 // Reset to 0 when AC button is clicked
 clearBtn.onclick = () => {
     numberArray = '';
     operatorArray = '';
-    previousNumber = '0';
+    previousNumber = undefined;
     currentNumber = '0';
-    displayVal.innerHTML = currentNumber;
+    displayVal.innerText = currentNumber;
 }
 
 // Remove right most digit when backspace button is clicked
@@ -45,8 +109,18 @@ backspaceBtn.onclick = () => {
     var numberLength = currentNumber.length;
     currentNumber = currentNumber.slice(0, numberLength - 1);
 
-    if (currentNumber === '') {
+    // if current number is empty, display zero instead of ''
+    if(currentNumber === '') {
         currentNumber = '0';
+    }
+
+    displayVal.innerText = currentNumber;
+}
+
+// Only allow one '.' on a number  
+decimalBtn.onclick = () => {
+    if(!currentNumber.includes('.')) {
+        currentNumber += '.';
     }
 
     displayVal.innerText = currentNumber;
